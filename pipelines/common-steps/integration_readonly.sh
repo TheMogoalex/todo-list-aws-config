9a45586d051c1e1a3d51668a883432dec589d214
TheMogoalex <amogollo2@alumno.uned.es>
2026-03-02 19:35:12 +0100

    creacion de venv

--- pipelines/common-steps/integration_readonly.sh
+++ pipelines/common-steps/integration_readonly.sh
@@ -8,7 +8,16 @@ export BASE_URL="$BASE_URL"
 
 mkdir -p reports
 
-source todo-list-aws/bin/activate
+if [[ ! -f "todo-list-aws/bin/activate" ]]; then
+  echo "Virtualenv not found. Creating it for read-only integration tests..."
+  rm -rf todo-list-aws
+  python3 -m venv todo-list-aws
+  source todo-list-aws/bin/activate
+  python -m pip install --upgrade pip
+  python -m pip install -q pytest requests unittest-xml-reporting
+else
+  source todo-list-aws/bin/activate
+fi
 
 python3 -m pytest -q -m readonly test/integration/todoApiTest.py \
   --junitxml=reports/integration-readonly-junit.xml
